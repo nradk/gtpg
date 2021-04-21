d108e8b754545d63d22c21e89acf0f4e0e50ddd9
Neeraj Adhikari <adh.neeraj@gmail.com>
2021-04-21 14:27:37 -0400

    Change webfont public path
    Change webfont public path so that it is served properly from gitlab
    pages.

--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,7 @@ module.exports = {
                     options: {
                         name: '[name].[ext]',
                         outputPath: 'webfonts',
-                        publicPath: '../webfonts',
+                        publicPath: 'webfonts',
                     },
                 }
             }
