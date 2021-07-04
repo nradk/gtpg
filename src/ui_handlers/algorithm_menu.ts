c485488e093cc48f6f901a0f782729241e1344bf
Neeraj Adhikari <adh.neeraj@gmail.com>
2021-07-04 16:48:53 -0400

    Reduce code duplication in algorithm menu handler
    Reduce code duplication in click handlers in algorithm_menu.ts

--- src/ui_handlers/algorithm_menu.ts
+++ src/ui_handlers/algorithm_menu.ts
@@ -17,162 +17,35 @@ import { ArticulationPoints } from "../algorithm/decompose/articulation";
 
 export default class AlgorithmUI {
 
+    static readonly algorithms = {
+        "#btn-algo-kruskal": [InputlessControls, KruskalMST],
+        "#btn-algo-prim": [InputlessControls, PrimMST],
+        "#btn-algo-bfs": [VertexInputControls, BreadthFirstSearch],
+        "#btn-algo-dfs": [VertexInputControls, DepthFirstSearch],
+        "#btn-algo-dijkstra": [VertexInputControls, DijkstrasShortestPath],
+        "#btn-algo-fleury": [InputlessControls, FleuryEulerTrail],
+        "#btn-algo-articulation": [InputlessControls, ArticulationPoints],
+        "#btn-algo-bhk": [InputlessControls, BHKHamiltonPath],
+        "#btn-algo-bhk-tsp": [InputlessControls, BHK_TSP],
+        "#btn-algo-nn-tsp": [InputlessControls, TSPApproxNearestNeighbor],
+        "#btn-algo-ni-tsp": [InputlessControls, TSPApproxNearestInsert],
+        "#btn-algo-ci-tsp": [InputlessControls, TSPApproxCheapestInsert],
+    };
+
     constructor(graphTabs: GraphTabs) {
+        for (const id in AlgorithmUI.algorithms) {
+            $(id).on('click', () => {
+                const graphDrawing = graphTabs.getActiveGraphDrawing();
+                if (graphDrawing == undefined) {
+                    alert("Please create or open a graph first.");
+                    return false;
+                }
+                const Controls = AlgorithmUI.algorithms[id][0];
+                const Algorithm = AlgorithmUI.algorithms[id][1];
+                const controls = new Controls(Algorithm, graphTabs, graphDrawing);
+                graphTabs.setControlPanelForActiveTab(controls);
 
-        $("#btn-algo-kruskal").on('click', () => {
-            const graphDrawing = graphTabs.getActiveGraphDrawing();
-            if (graphDrawing == undefined) {
-                console.error("No graph present for Kruskal Algorithm.");
-                alert("Please create or open a graph first to apply Kruskal's" +
-                    " Algorithm.");
-                return false;
-            }
-            const controls = new InputlessControls(KruskalMST,
-                graphTabs, graphDrawing);
-            graphTabs.setControlPanelForActiveTab(controls);
-        });
-
-        $("#btn-algo-prim").on('click', () => {
-            const graphDrawing = graphTabs.getActiveGraphDrawing();
-            if (graphDrawing == undefined) {
-                console.error("No graph present for Prim's Algorithm.");
-                alert("Please create or open a graph first to apply Prim's" +
-                    " Algorithm.");
-                return false;
-            }
-            const controls = new InputlessControls(PrimMST,
-                graphTabs, graphDrawing);
-            graphTabs.setControlPanelForActiveTab(controls);
-        });
-
-        $("#btn-algo-bfs").on('click', () => {
-            const graphDrawing = graphTabs.getActiveGraphDrawing();
-            if (graphDrawing == undefined) {
-                console.error("No graph present for BFS.");
-                alert("Please create or open a graph first to apply BFS" +
-                    " Algorithm.");
-                return false;
-            }
-            const controls = new VertexInputControls(BreadthFirstSearch,
-                graphTabs, graphDrawing);
-            graphTabs.setControlPanelForActiveTab(controls);
-        });
-
-        $("#btn-algo-dfs").on('click', () => {
-            const graphDrawing = graphTabs.getActiveGraphDrawing();
-            if (graphDrawing == undefined) {
-                console.error("No graph present for DFS.");
-                alert("Please create or open a graph first to apply DFS" +
-                    " Algorithm.");
-                return false;
-            }
-            const controls = new VertexInputControls(DepthFirstSearch,
-                graphTabs, graphDrawing);
-            graphTabs.setControlPanelForActiveTab(controls);
-        });
-
-        $("#btn-algo-dijkstra").on('click', () => {
-            const graphDrawing = graphTabs.getActiveGraphDrawing();
-            if (graphDrawing == undefined) {
-                console.error("No graph present for Dijkstra's Algorithm.");
-                alert("Please create or open a graph first to apply Dijkstra's" +
-                    " Algorithm.");
-                return false;
-            }
-            const controls = new VertexInputControls(DijkstrasShortestPath,
-                graphTabs, graphDrawing);
-            graphTabs.setControlPanelForActiveTab(controls);
-        });
-
-        $("#btn-algo-fleury").on('click', () => {
-            const graphDrawing = graphTabs.getActiveGraphDrawing();
-            if (graphDrawing == undefined) {
-                console.error("No graph present for Fleury's Algorithm.");
-                alert("Please create or open a graph first to apply Fleury's" +
-                    " Algorithm.");
-                return false;
-            }
-            const controls = new InputlessControls(FleuryEulerTrail,
-                graphTabs, graphDrawing);
-            graphTabs.setControlPanelForActiveTab(controls);
-        });
-
-        $("#btn-algo-articulation").on('click', () => {
-            const graphDrawing = graphTabs.getActiveGraphDrawing();
-            if (graphDrawing == undefined) {
-                console.error("No graph present for Articulation Points Algorithm.");
-                alert("Please create or open a graph first to apply Articulation Points" +
-                    " Algorithm.");
-                return false;
-            }
-            const controls = new InputlessControls(ArticulationPoints,
-                graphTabs, graphDrawing);
-            graphTabs.setControlPanelForActiveTab(controls);
-        });
-
-        $("#btn-algo-bhk").on('click', () => {
-            const graphDrawing = graphTabs.getActiveGraphDrawing();
-            if (graphDrawing == undefined) {
-                console.error("No graph present for Bellman-Held-Karp algorithm.");
-                alert("Please create or open a graph first to apply Bellman-Held-Karp" +
-                    " Algorithm.");
-                return false;
-            }
-            const controls = new InputlessControls(BHKHamiltonPath,
-                graphTabs, graphDrawing);
-            graphTabs.setControlPanelForActiveTab(controls);
-        });
-
-        $("#btn-algo-bhk-tsp").on('click', () => {
-            const graphDrawing = graphTabs.getActiveGraphDrawing();
-            if (graphDrawing == undefined) {
-                console.error("No graph present for Bellman-Held-Karp TSP algorithm.");
-                alert("Please create or open a graph first to apply Bellman-Held-Karp TSP" +
-                    " Algorithm.");
-                return false;
-            }
-            const controls = new InputlessControls(BHK_TSP,
-                graphTabs, graphDrawing);
-            graphTabs.setControlPanelForActiveTab(controls);
-        });
-
-        $("#btn-algo-nn-tsp").on('click', () => {
-            const graphDrawing = graphTabs.getActiveGraphDrawing();
-            if (graphDrawing == undefined) {
-                console.error("No graph present for Nearest Neighbor TSP approximation algorithm.");
-                alert("Please create or open a graph first to apply Nearest Neighbor TSP" +
-                    " Approximation Algorithm.");
-                return false;
-            }
-            const controls = new InputlessControls(TSPApproxNearestNeighbor,
-                graphTabs, graphDrawing);
-            graphTabs.setControlPanelForActiveTab(controls);
-        });
-
-        $("#btn-algo-ni-tsp").on('click', () => {
-            const graphDrawing = graphTabs.getActiveGraphDrawing();
-            if (graphDrawing == undefined) {
-                console.error("No graph present for Nearest Insert TSP approximation algorithm.");
-                alert("Please create or open a graph first to apply Nearest Insert TSP" +
-                    " Approximation Algorithm.");
-                return false;
-            }
-            const controls = new InputlessControls(TSPApproxNearestInsert,
-                graphTabs, graphDrawing);
-            graphTabs.setControlPanelForActiveTab(controls);
-        });
-
-        $("#btn-algo-ci-tsp").on('click', () => {
-            const graphDrawing = graphTabs.getActiveGraphDrawing();
-            if (graphDrawing == undefined) {
-                console.error("No graph present for Cheapest Insert TSP approximation algorithm.");
-                alert("Please create or open a graph first to apply Cheapest Insert TSP" +
-                    " Approximation Algorithm.");
-                return false;
-            }
-            const controls = new InputlessControls(TSPApproxCheapestInsert,
-                graphTabs, graphDrawing);
-            graphTabs.setControlPanelForActiveTab(controls);
-        });
+            });
+        }
     }
 }
